bf17237b79178138e1fe1424ce09684e6efa727f
FireLord1 <firelord.xda@gmail.com>
2017-02-25 01:33:59 +0530

    treltektt: for now use ss333 modem for ril
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --d
 TARGET_KERNEL_CONFIG := cm_exynos5433-treltektt_defconfig
 
 # RIL
-BOARD_MODEM_TYPE := ss300
+BOARD_MODEM_TYPE := ss333
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := treltektt
